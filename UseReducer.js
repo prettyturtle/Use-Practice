8b4489376cd69b4387725351c8bb13e07539c601
prettyturtle <unifconti@gmail.com>
2023-02-02 00:00:44 +0900

    useReducer(3)

--- UseReducer.js
+++ UseReducer.js
@@ -1,10 +1,12 @@
 import { useState, useReducer } from "react"
 import { StyleSheet, View, Text, Button, TextInput } from "react-native"
+import Divider from "./Divider"
 
 // reducer  : state를 업데이트 하는 역할
 // dispatch : state 업데이트를 위한 요구
 // action   : 요구의 내용
 
+// ============== (1) ==============
 const ACTION_TYPES = {
   deposit: "deposit",
   withdraw: "withdraw",
@@ -23,9 +25,46 @@ const reducer = (state, action) => {
   }
 }
 
+// ============== (2) ==============
+const NAME_LIST_ACTION_TYPES = {
+  add: "add",
+  check: "check",
+  delete: "delete",
+}
+
+const nameListReducer = (state, action) => {
+  console.log(state, action)
+  switch (action.type) {
+    case NAME_LIST_ACTION_TYPES.add:
+      if (state.filter((user) => action.user.name === user.name).length === 0) {
+        return state.concat([action.user])
+      }
+      return state
+    case NAME_LIST_ACTION_TYPES.check:
+      return state.map((item, _) => {
+        if (item.name === action.user.name) {
+          return {
+            ...item,
+            isCheck: !item.isCheck,
+          }
+        }
+        return { ...item }
+      })
+    case NAME_LIST_ACTION_TYPES.delete:
+      return state.filter((user) => user.name !== action.user.name)
+    default:
+      return state
+  }
+}
+
 const UseReducer = () => {
+  // ============== (1) ==============
   const [number, setNumber] = useState(0)
   const [money, dispatch] = useReducer(reducer, 0)
+
+  // ============== (2) ==============
+  const [input, setInput] = useState("")
+  const [nameList, nameListDispatch] = useReducer(nameListReducer, [])
 
   return (
     <View style={{ alignItems: "center", width: "100%", marginBottom: 100 }}>
@@ -39,6 +78,7 @@ const UseReducer = () => {
       >
         [useReducer]
       </Text>
+      {/* ============== (1) ============== */}
       <Text>useReducer은행에 오신 것을 환영합니다</Text>
       <Text>잔고 : {money} 원</Text>
       <TextInput
@@ -75,6 +115,74 @@ const UseReducer = () => {
             })
           }}
         />
+      </View>
+
+      {/* ============== (2) ============== */}
+      <View style={{ flex: 1 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            width: "100%",
+            alignItems: "center",
+          }}
+        >
+          <TextInput
+            value={input}
+            placeholder="이름을 입력하세요..."
+            onChangeText={setInput}
+            style={{
+              borderRadius: 12,
+              height: 48,
+              marginHorizontal: 16,
+              flex: 1,
+              borderColor: "lightgrey",
+              borderWidth: 0.4,
+              fontSize: 16,
+              paddingHorizontal: 8,
+            }}
+          />
+          <Button
+            title="등록"
+            onPress={() => {
+              nameListDispatch({
+                type: NAME_LIST_ACTION_TYPES.add,
+                user: {
+                  name: input,
+                  isCheck: false,
+                },
+              })
+            }}
+          />
+        </View>
+        <View>
+          {nameList.map((item, index) => {
+            return (
+              <>
+                <Text>{index}번째</Text>
+                <Button
+                  color={item.isCheck ? "blue" : "black"}
+                  title={item.name}
+                  onPress={() => {
+                    nameListDispatch({
+                      type: NAME_LIST_ACTION_TYPES.check,
+                      user: item,
+                    })
+                  }}
+                />
+                <Button
+                  title="삭제"
+                  onPress={() => {
+                    nameListDispatch({
+                      type: NAME_LIST_ACTION_TYPES.delete,
+                      user: item,
+                    })
+                  }}
+                />
+                <Divider />
+              </>
+            )
+          })}
+        </View>
       </View>
     </View>
   )
